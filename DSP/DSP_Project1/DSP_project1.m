8c42b7dbfbaac34464e49cfc8a8fdcf1e2cbcfd9
ceegeechow <camille@chowww.com>
2017-10-05 15:12:50 -0400

    finished project

--- DSP/DSP_Project1/DSP_project1.m
+++ DSP/DSP_Project1/DSP_project1.m
@@ -1,7 +1,12 @@
+%Camille Chow
+%ECE-310-B
+%Sampling Rate Conversion Project
+%10/9/17
+
 [x,Fs] = audioread('Wagner.wav');
-y = srconvert3(x);
+y = srconvert(x);
 sound(y,24000)
 
 impulse = [1 zeros(1,3000)];
-h = srconvert3(impulse);
+h = srconvert(impulse);
 verify(h)
--- DSP/DSP_Project1/srconvert.m
+++ DSP/DSP_Project1/srconvert.m
@@ -1,15 +1,44 @@
 function y = srconvert(in)
-%[x,Fs] = audioread(in);
 
-%upsample input
-w1 = upsample(in,320);
+%make sure input is row vector
+s = size(in);
+if s(2) == 1
+    in = in.';
+end
 
-%pass through filter
-Hd = getFilter;
-%impulse response of filter
-%impulse = [1 zeros(1,10000)];
-%h = filter(Hd,impulse);
-w2 = filter(Hd,w1);
+%upsampling filters
+Hd2 = getFilterFIR(2);
+Hd5 = getFilterFIR(5);
+
+%impulse responses
+impulse = [1 zeros(1,3000)];
+h2 = filter(Hd2,impulse);
+h5 = filter(Hd5,impulse);
+
+%polyphase matrices
+E2 = poly1(h2,2);
+E5 = poly1(h5,5);
+
+%filter signal through each row of E2, then upsample by 2, delay,
+%and sum components
+for i = 1:6
+    s = size(upsample(in,2));
+    w2 = zeros(2,s(2));
+    for j = 1:2
+        w2(j,:) = circshift(upsample(fftfilt(E2(j,:),in),2),j-1);
+    end
+    in = sum(w2);
+end
+%filter through E5, upsample, delay, and sum
+s = size(upsample(in,5));
+w5 = zeros(5,s(2));
+for i = 1:5
+    w5(i,:) = circshift(upsample(fftfilt(E5(i,:),in),5),i-1);
+end
+in = sum(w5);
 
 %downsample
-y = downsample(w2,147);
+in = downsample(in,147);
+
+%magnify signal to account for attenuation
+y = 1000*in;
