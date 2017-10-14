ab587533be118d389771c242c20c9c1df838be23
ceegeechow <camille@chowww.com>
2017-10-13 20:35:54 -0400

    project submitted

--- DSP/DSP_Project1/srconvert2.m
+++ DSP/DSP_Project1/srconvert2.m
@@ -5,7 +5,7 @@ Hd5 = getFilterFIR(5);
 %downsampling filters
 Hd3 = getFilterFIR(3);
 Hd7 = getFilterFIR(7);
-
+tic;
 %upsample and filter by 2 six times
 for i = 1:6
     in = upsample(in,2);
@@ -15,13 +15,8 @@ end
 in = upsample(in,5);
 in = filter(Hd5,in);
 
-%downsample and filter by 3 once
-in = filter(Hd3,in);
-in = downsample(in,3);
-%downsample and filter by 7 twice
-for i = 1:2
-    in = filter(Hd7,in);
-    in = downsample(in,7);
-end
+in = downsample(in,147);
+
+toc
 
 y = 1000*in;
--- DSP/DSP_Project1/srconvert_bad.m
+++ DSP/DSP_Project1/srconvert_bad.m
@@ -1,15 +1,18 @@
-function y = srconvert(in)
+function y = srconvert_bad(in)
 %[x,Fs] = audioread(in);
 
 %upsample input
 w1 = upsample(in,320);
 
 %pass through filter
-Hd = getFilter;
+Hd = getFilter(320);
 %impulse response of filter
 %impulse = [1 zeros(1,10000)];
 %h = filter(Hd,impulse);
+tic;
 w2 = filter(Hd,w1);
 
 %downsample
-y = downsample(w2,147);
+y = 1000*downsample(w2,147);
+
+toc
