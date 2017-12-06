31a42bc6bf9d31c2c741825d24f000bd5484600e
ceegeechow <camille@chowww.com>
2017-12-06 12:38:19 -0500

    getting closer

--- DSP/DSP_Project3/DSP_project3.m
+++ DSP/DSP_Project3/DSP_project3.m
@@ -2,17 +2,17 @@
 %ECE-310-B
 %Spectrogram Analysis and Applications
 %12/11/17
-close all;
-%% Part 1
+
+%% Part 1 - generating chirp and plotting spectrogram
 N = 256;
 fs = 5e6;
 mu = 4e9;
 t = (0:1/fs:200e-6);
-x = cos(2*pi*mu*(t.^2));
+chirp = cos(2*pi*mu*(t.^2));
 window = triang(N);
-spectrogram(x,window,255,N,'yaxis')
+spectrogram(chirp,window,255,N,'yaxis')
 
-%% Part 2
+%% Part 2 - instantaneous frequency
 figure
 f1 = mu*t;
 f2 = 2*mu*t;
@@ -25,7 +25,7 @@ ylabel('w (rad)')
 legend('w1','w2')
 %according to plot, f1 corresponds to slope of ridge
 
-%% Part 3
+%% Part 3 - new chirp rate
 figure
 mu2 = 1e10;
 x2 = cos(2*pi*mu2*(t.^2));
@@ -33,7 +33,7 @@ spectrogram(x2,window,255,N,'yaxis')
 %if mu becomes too large, the chirp frequency will exceed the
 %Nyquist frequency, which is why the rigde begins to slope down
 
-%% Part 4
+%% Part 4 - narrowband spectrogram
 fftlen = 256;
 winlen = 16;
 overlap = 15;
@@ -46,12 +46,12 @@ spectrogram(s1,w,overlap,fftlen,'yaxis')
 subplot(2,1,2)
 spectrogram(s5,w,overlap,fftlen,'yaxis')
 
-%% Part 5
+%% Part 5 - wideband spectrogram
 fftlen = 512;
 winlen = 512;
 overlap = 511;
-load('s1.mat');
-load('s5.mat');
+% load('s1.mat');
+% load('s5.mat');
 w = triang(winlen);
 figure
 subplot(2,1,1)
@@ -59,30 +59,29 @@ spectrogram(s1,w,overlap,fftlen,'yaxis')
 subplot(2,1,2)
 spectrogram(s5,w,overlap,fftlen,'yaxis')
 
-%% Part 6
+%% Part 6 - ISTFT
 load('vowels.mat');
 s1 = spectrogram(vowels,rectwin(256),128,1023);
-%complete spectrogram
 s2 = flipud(s);
-s = [s1;s2];
+s = [s1;s2]; %complete spectrogram
 y = invSTFT(s);
-x1 = (0:1:length(vowels));
-x2 = (0:1:length(y));
+x1 = (0:1:length(vowels)-1);
+x2 = (0:1:length(y)-1);
 figure
 subplot(2,1,1)
-stem(vowels)
+plot(x1,vowels)
 subplot(2,1,2)
-stem(real(y))
+plot(x2,y)
 title('Input vs Output Signal')
 
-%% Part 7
+%% Part 7 - changing rate of speech
 s = downsample(s,2);
 y = invSTFT(s);
 figure
 subplot(2,1,1)
 stem(vowels)
 subplot(2,1,2)
-stem(real(y))
+stem(y)
 title('Original vs Modified Signal')
-sound(vowels,8000)
-sound(y,8000)
+%sound(vowels,8000)
+%sound(y,8000)
--- DSP/DSP_Project3/invSTFT.m
+++ DSP/DSP_Project3/invSTFT.m
@@ -4,16 +4,16 @@ sz = size(s);
 n = sz(2);
 m = 128*(n+1);
 inv = zeros(n,m);
-% %initialize row vector to store ifft
-% r = zeros(1,m);
+
 %compute ifft's
 for i = 1:n
     r = ifft(s(:,i));
-    a = 128*(i-1);
+    a = 128*(i-1) + 1;
     b = a + 256;
     inv(i,a:b) = (r(1:257,1)).';
 end
 y = sum(inv);
 y2 = y(129:length(y)-128);
 y(129:length(y)-128) = y2/2;
+y = real(y);
 end
